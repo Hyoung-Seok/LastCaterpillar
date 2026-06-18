93a476d5291ff2aef24770de75acbff850e520d5
Hyoung-Seok <dkavmdk98@gmail.com>
2026-06-18 23:44:31 +0900

    [P] 플레이어 회전 구현

--- Assets/2_Scripts/Player/PlayerMoveController.cs
+++ Assets/2_Scripts/Player/PlayerMoveController.cs
@@ -7,17 +7,24 @@ public class PlayerMoveController : MonoBehaviour
     [Header("Component")] 
     [SerializeField] private Rigidbody rb;
 
-    [Header("Move Config")] 
+    [Header("Forward/Revers Config")] 
     [SerializeField] private float forwardMaxSpeed;
     [SerializeField, Range(-100, -1)] private float reverseMaxSpeed;
     [SerializeField] private float acceleration;
     [SerializeField] private float breakDecel;
     [SerializeField] private float deceleration;
+    
+    [Header("Turn Config")]
+    [SerializeField] private float turnSpeed;
+    [SerializeField] private float turnBreakDecel;
+    [SerializeField] private float turnAcceleration;
+    [SerializeField] private float turnDeceleration;
 
     private GameInput _gameInput;
     private InputAction _move;
     private Vector2 _moveInput;
     private float _curSpeed = 0f;
+    private float _curTurnSpeed = 0f;
     
     private void Awake()
     {
@@ -30,11 +37,15 @@ public class PlayerMoveController : MonoBehaviour
         _moveInput = _move.ReadValue<Vector2>();
         
         UpdateSpeed();
+        UpdateTurnSpeed();
     }
 
     private void FixedUpdate()
     {
         rb.linearVelocity = transform.forward * _curSpeed;
+
+        var angle = _curTurnSpeed * Time.fixedDeltaTime;
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(0, angle, 0));
     }
 
     private void UpdateSpeed()
@@ -63,13 +74,34 @@ public class PlayerMoveController : MonoBehaviour
         _curSpeed = Mathf.MoveTowards(_curSpeed, targetSpeed, rate * Time.deltaTime);
     }
 
-    private void OnEnable()
+    private void UpdateTurnSpeed()
     {
-        _gameInput.Enable();
+        var turnDir = Math.Sign(_moveInput.x);
+        var turnSpeedDir = Math.Sign(_curTurnSpeed);
+        var targetSpeed = 0f;
+        var rate = 0f;
+        
+        if (turnDir == 0)
+        {
+            targetSpeed = 0f;
+            rate = turnDeceleration;
+        }
+        else if (turnSpeedDir != 0 && turnDir != turnSpeedDir)
+        {
+            targetSpeed = 0f;
+            rate = turnBreakDecel;
+        }
+        else
+        {
+            targetSpeed = turnDir * turnSpeed;
+            rate = turnAcceleration;
+        }
+        
+        _curTurnSpeed = Mathf.MoveTowards(_curTurnSpeed, targetSpeed, rate * Time.deltaTime);
     }
 
-    private void OnDisable()
-    {
-        _gameInput.Disable();
-    }
+    private void OnEnable() => _gameInput.Enable();
+    
+    private void OnDisable() => _gameInput.Disable();
+    
 }
