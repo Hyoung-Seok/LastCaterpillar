6b61690b3808612aacb2b6e75325e71a89c4a778
Hyoung-Seok <dkavmdk98@gmail.com>
2026-06-27 01:36:51 +0900

    [E] 적들이 분리되어 움직일 수 있도록 조향 기능 추가

--- Assets/2_Scripts/Enemy/DummyEnemy.cs
+++ Assets/2_Scripts/Enemy/DummyEnemy.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class DummyEnemy : MonoBehaviour
 {
-    [SerializeField] private FlowField flowField;
+    [HideInInspector] public Vector3 Separation;
+    [HideInInspector] public FlowField FlowField;
+    
+    [SerializeField] private float flowWeight = 1f;
+    [SerializeField] private float separationWeight = 0.5f;
+    
     [SerializeField] private CharacterController cc;
     [SerializeField] private float speed = 5f;
 
+    
     private void Update()
     {
-        var cellDir = flowField.GetCurrentCellDirection(transform.position);
-        var dir = new Vector3(cellDir.x, 0, cellDir.y).normalized;
+        var cellDir = FlowField.GetCurrentCellDirection(transform.position);
+        var flow = new Vector3(cellDir.x, 0, cellDir.y).normalized;
+
+        var dir = flow * flowWeight + Separation * separationWeight;
         
-        cc.Move(dir * (speed * Time.deltaTime));
+        cc.Move(dir.normalized * (speed * Time.deltaTime));
     }
 }
