b733dad2741b2be96e86326abe052d0f4b637d40
Hyoung-Seok <dkavmdk98@gmail.com>
2026-08-01 23:27:36 +0900

    [P] 레티클 컨트롤러 구현

--- Assets/2_Scripts/Player/MainTurret.cs
+++ Assets/2_Scripts/Player/MainTurret.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(PlayerManager))]
-public class MainTurrent : MonoBehaviour
+public class MainTurret : MonoBehaviour
 {
-    [Header("Components")]
+    [Header("Components")] 
+    [SerializeField] private ReticleController reticle;
     [SerializeField] private Transform turret;
     [SerializeField] private Transform firePos;
     
@@ -16,7 +17,7 @@ public class MainTurrent : MonoBehaviour
     
     private InputReader _inputReader;
     private InputAction _fire;
-    private Shell _currentRoadedShell;
+    private Shell _currentLoadedShell;
 
     private bool _isReloading = false;
     private float _curReloadTime = 0f;
@@ -30,14 +31,17 @@ public class MainTurrent : MonoBehaviour
         _fire.performed += FireMainTurret;
         pm.OnDisableTurret += OnDisableTurret;
         
-        _currentRoadedShell = shell;
+        _currentLoadedShell = shell;
     }
 
     private void Update()
     {
+        reticle.UpdateReticleState(IsCanFire());
+        
         if (_isReloading == false) return;
         
         _curReloadTime += Time.deltaTime;
+        
         if (_curReloadTime >= reloadTime)
         {
             _isReloading = false;
@@ -45,7 +49,7 @@ public class MainTurrent : MonoBehaviour
         }
     }
 
-    public void ChangeShellType(ShellType type)
+    public void ChangeShell()
     {
         
     }
@@ -56,21 +60,26 @@ public class MainTurrent : MonoBehaviour
 
         _isReloading = true;
 
-        var s = Instantiate(_currentRoadedShell, firePos.position, Quaternion.identity);
+        var s = Instantiate(_currentLoadedShell, firePos.position, Quaternion.identity);
         s.OnStartFire(firePos.position, _inputReader.AimPoint);
     }
 
     private bool IsCanFire()
     {
         // 현재 장전된 탄약이 없거나 장전중이라면 false
-        if (_currentRoadedShell == null || _isReloading)
+        if (_currentLoadedShell == null || _isReloading)
             return false;
 
-        var dir = _inputReader.AimPoint - firePos.position;
-        dir.y = 0;
-        dir.Normalize();
+        var toAim = _inputReader.AimPoint - firePos.position;
+        toAim.y = 0;
+
+        var minDist = _currentLoadedShell.MinFireDistance;
+        // 포탄이 너무 가까우면 
+        if (toAim.sqrMagnitude < minDist * minDist)
+            return false;
         
-        var dot = Vector3.Dot(firePos.forward, dir);
+        toAim.Normalize();
+        var dot = Vector3.Dot(firePos.forward, toAim);
         
         // 포탑이 발사 지점을 바라보고 있지 않다면 false
         return dot > fireThreshold;
---
+++ Assets/2_Scripts/Weapon/ReticleController.cs
@@ -0,0 +1,60 @@
+using System;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class ReticleController : MonoBehaviour
+{
+    [Header("Components")] 
+    [SerializeField] private InputReader inputReader;
+    [SerializeField] private DecalProjector decal;
+
+    [Header("Config")] 
+    [SerializeField] private float yPos;
+    [ColorUsage(true, true), SerializeField] private Color enableColor;
+    [ColorUsage(true, true), SerializeField] private Color disableColor;
+
+    private static readonly int COLOR = Shader.PropertyToID("_Color");
+    private bool _prevState;
+    
+    private Material _enableMat;
+    private Material _disableMat;
+
+    private void Awake()
+    {
+        _enableMat = new Material(decal.material);
+        _disableMat = new Material(decal.material);
+        
+        _enableMat.SetColor(COLOR, enableColor);
+        _disableMat.SetColor(COLOR, disableColor);
+        
+        decal.material = _disableMat;
+    }
+
+    private void LateUpdate()
+    {
+        var p = inputReader.AimPoint;
+        transform.position = new Vector3(p.x, yPos, p.z);
+    }
+
+    public void UpdateReticleState(bool isEnable)
+    {
+        if (_prevState == isEnable) return;
+        
+        _prevState = isEnable;
+        decal.material = isEnable ? _enableMat : _disableMat;
+    }
+
+    public void SetReticleSize(float radius)
+    {
+        var s = decal.size;
+        s.x = s.y = radius * 2f;
+        
+        decal.size = s;
+    }
+
+    private void OnDestroy()
+    {
+        if(_enableMat != null) Destroy(_enableMat);
+        if(_disableMat != null) Destroy(_disableMat);
+    }
+}
