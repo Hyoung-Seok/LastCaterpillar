77ea1b4c4a97f6995f34e679afd33a01ddaf2548
Hyoung-Seok <dkavmdk98@gmail.com>
2026-08-05 03:28:44 +0900

    [E] 군중 타입의 적 클래스 구현

--- Assets/2_Scripts/Enemy/Enemy.cs
+++ Assets/2_Scripts/Enemy/Enemy.cs
@@ -4,22 +4,25 @@ using UnityEngine;
 public abstract class Enemy : MonoBehaviour, ISpatialItem
 {
     public Vector3 Position => transform.position;
-    public virtual float InfluenceRadius => influenceRadius;
+    public float InfluenceRadius => influenceRadius;
 
     [Header("Config")] 
     [SerializeField] protected float hp;
     [SerializeField] protected float speed;
     [SerializeField] protected float rotationSpeed;
     
-    [Header("Seperation config")]
+    [Header("Spatial config")]
     [SerializeField] protected float influenceRadius;
 
     public event Action<Enemy> OnDeadEvent;
+    
+    private bool _isDead = false;
 
     public void TakeDamage(float damage)
     {
+        if(_isDead) return;
+        
         hp -= damage;
-
         if (hp > 0) return;
         
         OnDead();
@@ -27,16 +30,22 @@ public abstract class Enemy : MonoBehaviour, ISpatialItem
     
     protected virtual void OnDead()
     {
+        _isDead = true;
         OnDeadEvent?.Invoke(this);
     }
+    
+    protected virtual void OnSpawned() { _isDead = false; }
+    protected virtual void OnDespawned() { _isDead = true; }
 
     private void OnEnable()
     {
-        
+        // 등록하기
+        OnSpawned();
     }
 
     private void OnDisable()
     {
-        
+        // 해제하기
+        OnDespawned();
     }
 }
---
+++ Assets/2_Scripts/Enemy/SwarmEnemy.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+public class SwarmEnemy : Enemy
+{
+    [HideInInspector] public Vector3 Separation;
+    [HideInInspector] public Vector3 ObstacleForce;
+    [HideInInspector] public FlowField FlowField;
+    
+    [SerializeField] private CharacterController cc;
+    [SerializeField] private float maxSeparation = 0.4f;
+    [SerializeField] private float maxObstacleForce = 0.8f;
+    
+    private float _bodyRadius;
+    
+    private void Start()
+    {
+        _bodyRadius = cc.radius;
+    }
+
+    private void Update()
+    {
+        var cellDir = FlowField.GetCurrentCellDirection(transform.position);
+        var flowVec = new Vector3(cellDir.x, 0, cellDir.y).normalized;
+        var step = speed * Time.deltaTime;
+        var pos = transform.position;
+        
+        if (FlowField.IsBlocked(pos))
+        {
+            cc.Move(flowVec * step);
+            return;
+        }
+
+        var force = Vector3.ClampMagnitude(Separation, maxSeparation) +
+                    Vector3.ClampMagnitude(ObstacleForce, maxObstacleForce);
+        
+        var desired = (flowVec + force).normalized * step;
+        cc.Move(SlideAlongWalls(pos, desired));
+        
+        if(flowVec.sqrMagnitude > 0.0001f)
+            RotationMoveDir(flowVec);
+    }
+
+    private Vector3 SlideAlongWalls(Vector3 pos, Vector3 desired)
+    {
+        var probeX = pos + ProbeOffset(desired.x, 0);
+        var probeZ = pos + ProbeOffset(0, desired.z);
+        
+        var blockX = FlowField.IsBlocked(probeX);
+        var blockZ = FlowField.IsBlocked(probeZ);
+
+        if (blockX) desired.x = 0;
+        if (blockZ) desired.z = 0;
+
+        return desired;
+    }
+
+    private void RotationMoveDir(Vector3 dir)
+    {
+        var target = Quaternion.LookRotation(dir);
+        transform.rotation = Quaternion.RotateTowards
+            (transform.rotation, target, Time.deltaTime * rotationSpeed);
+    }
+    
+    private Vector3 ProbeOffset(float dx, float dz)
+    {
+        return new Vector3(
+            dx != 0 ? Mathf.Sign(dx) * (Mathf.Abs(dx) + _bodyRadius) : 0,
+            0,
+            dz != 0 ? Mathf.Sign(dz) * (Mathf.Abs(dz) + _bodyRadius) : 0);
+    }
+}
