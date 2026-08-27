f76301736f69dccc2174bad8bdd73e830e9b3a3e
Hyoung-Seok <dkavmdk98@gmail.com>
2026-08-28 01:48:27 +0900

    [E] 적이 순차적으로 스폰될 수 있도록 변경

--- Assets/2_Scripts/Enemy/WaveManager.cs
+++ Assets/2_Scripts/Enemy/WaveManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,22 +8,37 @@ public class WaveManager : MonoBehaviour
     [SerializeField] private List<Enemy> enemy;
 
     [Header("Config")] 
+    [SerializeField] private int spawnRepeatCount;
     [SerializeField] private int spawnCount;
+    [SerializeField] private float spawnInterval;
+    
+    private IEnumerator _spawnRoutine;
     
     public void Start()
     {
-        SpawnEnemy();
+        StartSpawn();
     }
 
-    public void SpawnEnemy()
+    public void StartSpawn()
     {
-        for (var i = 0; i < spawnCount; ++i)
+        _spawnRoutine = StartSpawnCoroutine();
+        StartCoroutine(_spawnRoutine);
+    }
+
+    private IEnumerator StartSpawnCoroutine()
+    {
+        for(var a = 0; a < spawnRepeatCount; ++a)
         {
-            var index = Random.Range(0, enemy.Count);
-            var e = Instantiate(enemy[index], transform);
+            for (var i = 0; i < spawnCount; i++)
+            {
+                var index = Random.Range(0, enemy.Count);
+                var e = Instantiate(enemy[index], transform);
             
-            e.transform.position = transform.position + new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f));
-            e.gameObject.SetActive(true);
+                e.transform.position = transform.position + new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f));
+                e.gameObject.SetActive(true);
+            }
+            
+            yield return new WaitForSeconds(spawnInterval);
         }
     }
 }
